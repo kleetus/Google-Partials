b3be95fd812b0f99fe854458e461a495a6253886
Chris Kleeschulte <laconia@gmail.com>
2012-02-26 11:23:08 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Googlepartials::Application.config.secret_token = '862ba32048c6e762cf5513154a215a11b2489a75d404a6aec58f1e8d61dfc18e3e0729f36a23b9e99a5aee8dca5f13bcfb35b4f4ff9473bf4552441a4180f06a'
